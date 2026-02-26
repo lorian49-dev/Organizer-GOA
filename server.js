4407e82893d868136069f4b8ed8ddfd3285e4de7
David Alejandro Gutierrez <120154242+lorian49-dev@users.noreply.github.com>
2026-02-26 16:10:37 -0500

    Pulicion y reforzado de conocimientos previos

--- server.js
+++ server.js
@@ -1,244 +1,285 @@
-  const path = require('path');
-  require('dotenv').config({path: path.join(__dirname, 'config.env')});
-  const {createClient} = require('@supabase/supabase-js');
-  const express = require('express');
-  const app = express();
-  const multer = require('multer');
+//-------------------------------------------------//
+//            Declaracion de variables
+//-------------------------------------------------//
 
+// constantes nativas
+const path = require('path');
+// dotenv
+require('dotenv').config({path: path.join(__dirname, 'config.env')})
+// constantes externas
+const multer = require('multer');
+const express = require('express');
+const {createClient} = require('@supabase/supabase-js');
+// instanciables
+const app = express();
 
-  // Almacenamiento en memoria 
-  // it's named memory storage because its function is save process in RAM memory
+// Almacenamiento en la memoria local
+const myStorage = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fieldSize: 10 * 1024 * 1024
+  }
+});
 
-  const upload = multer({
-    storage: multer.memoryStorage(),
-    limits: {
-      fieldSize: 10 * 1024 * 1024
-    }
-  });
-
-  // acciones
-
-  const supabase = createClient(
+// Acceso con credenciales por medio de dotenv
+const access = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_KEY
+)
 
-  );
+//-------------------------------------------------//
+//                   COOKIES
+//-------------------------------------------------//
 
-  // Aplicacion, GET, POST & LISTEN
-
-  app.use(express.static(path.join(__dirname, 'public')))
-  app.use('/logo-types',express.static(path.join(__dirname, 'src/logo-types')))
-
-  app.get('/aceptar-cookie', (req, res)=>{
-     res.cookie('cookie-aceptada', 'true', {
-      maxAge: 10080000,
-      httpOnly: false
-     })
-     res.status(200).send('Entendido')
-  });
-
-// \/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
-// --------------------------------------------
-// Solicitud de informacion a la tabla INVOICES
-// --------------------------------------------
-// /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
-
-  app.get('/invoices-table-get', async(req, res)=>{
-    const q = parseInt(req.query.page)
-    const page = Number.isInteger(q) && q > 0 ? q : 1;
-    const limit = 10
-
-    const start = (page - 1) * limit;
-    const end = start + (limit - 1);
-
-try{
-
-   const {data, error, count} = await supabase.from('invoices').select('id_invoice, name, weight, date, url', {count:'exact'}).order('date', {ascending : false}).range(start, end);
-
-  const totalRows = Math.ceil(count/limit)
-
-  if(error){
-    throw error;
-  }
-
-
-  res.json({data, totalRows})
-} catch(err){
-  console.error(err);
-      res.status(500).json({error:'Error al solicitar informacion'})
-}
+app.get('/aceptar-cookie', (req, res)=>{
+  res.cookie('cookie-aceptada', true, {
+    maxAge:3600000,
+    httpOnly:false
   })
+  res.status(200).send('cookie adicionada con exito')
+})
 
-  app.post('/upload', (req, res)=>{
-    upload.array('pdf', 15)(req,res,async(err)=>{
+//-------------------------------------------------//
+//              manejo de la app
+//-------------------------------------------------//
 
-      if(err){
-        return res.status(400).json({
-          error: 'Solo se permiten 15 archivos'
-        })
-      }
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/logo-types', express.static(path.join(__dirname, 'src/logo-types')));
 
-    try {
-      // se crea una instancia de la aplicacion, cuando se ejecute el metodo post a dicha direccion, req.file tendra lugar
-      const files = req.files;
-      // medida de seguridad por si llegase a fallar o no haya ningun archivo que procesar
-      if(!files || files.length === 0){
-      return res.status(400).json({error: 'No file uploaded'});
-      }
-      const promesa = files.map( async file =>{
+// obtencion de datos INVOICES
 
-      // el nombre del archivo que se subira
-      const fileName = Date.now() +'-'+ file.originalname;
+app.get('/invoices-table-get', async(req, res)=>{
+  const p = parseInt(req.query.page);
+  const page = Number.isInteger(p) && p > 1 ? p : 1;
+  const limit = 10;
 
-      // subida a Supabase
+  // inicio y fin para e rango en la paginacion
 
-      const {data, error} = await supabase.storage.from('invoices').upload(fileName, file.buffer, {contentType: file.mimetype});
-      if(error){
-        throw error;
-      }
+  const start = (page - 1) * limit;
+  const end = start + (limit - 1);
 
-      // Obtener la URL publica
+  try{
 
-      const {data: publicUrlData} = await supabase.storage.from('invoices').getPublicUrl(fileName);
-
-      const publicUrl = publicUrlData.publicUrl;
-
-      return{
-        name : file.originalname,
-        weight: file.size,
-        date: new Date(),
-        url: publicUrl
-      }
-      
-      })
-const resultados = await Promise.all(promesa);
-     await supabase.from('invoices').insert(resultados)
-      res.redirect('/sections/loadFile.html')
-    } catch(err){
-      console.error(err);
-      res.status(500).json({error:'Error al subir Arcvhivo'})
-    }
-        })
-  })
-
-  app.delete('/action-delete/:id', async(req, res)=>{
-    const idDelete = req.params.id;
-    
-    const {data, error} =  await supabase.from('invoices').delete().eq('id_invoice', idDelete)
+    const {data, error, count} = await access.from('invoices').select('id_invoice, name, weight, date, url', {count:'exact'}).order('date', {ascending: false}).range(start, end);
 
     if(error){
-      return res.status(400).json({error: error.message})
+      throw error
     }
 
-    res.status(200).send({mensaje:'Registro borrado exitosamente'})
-   
-  })
+    const totalRows = Math.ceil(count / limit);
 
-// \1/\1/\1/\1/\1/\1/\1/\1/\1/\1/\1/\1/\1/\1/\1/
-// --------------------------------------------
-// Solicitud de informacion a la tabla MANIFEST
-// --------------------------------------------
-// /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
+    res.json({data, totalRows});
 
-// Obtencion de datos de la tabla de invoices
+  }catch(error){
+    console.error(error)
+    res.status(500).json({error: 'error en la obtencion de datos'})
+  }
+
+})
+
+// obtencion de datos PARA TABLA MANIFEST
 
 app.get('/manifest-table-get', async(req, res)=>{
- // -------_____Obtencion de datos de la url_____--------
-const urlPage = parseInt(req.query.page)
-const page = Number.isInteger(urlPage) && urlPage > 1 ? urlPage : 1;
-const limit = 10;
+  const p = parseInt(req.query.page);
+  const page = Number.isInteger(p) && p > 1 ? p : 1;
+  const limit = 10;
 
-// Defnicion de Limites para el rango de las tablas a medida que cambie la url 
+  // inicio y fin para e rango en la paginacion
 
-const start = (page - 1) * limit; // Esto funciona de la forma en que, si la pagina en la url vale 1(el cual es el menor segun las reglas anteriormente planteadas), al restarle 1 quedaria 0, eso se multiplica por el limite y dara 0, el comienzo seria 0.
-// caso contrario si valiese pagina 2, se le resta 1, y se multiplica por 10, dara como comienzo la posicion 10.
-const end = start + (limit - 1);
-// Similar al comienzo, estedebe tomarlo como base, sumarle el limite y restarle un digito, esto porque recordemosque en prograacion la numeracion no comienza desde el 1, sino desde el 0. Entonces, si quiero contar desde el 0 hasta el 10, estaria contando 11 digitos.
+  const start = (page - 1) * limit;
+  const end = start + (limit - 1);
 
-// La siguiente linea sera el llamado de los datos de la tabla de la base de datos, utilizaremos supabase ya que desde hay fue que se almaceno su conexion.
-// se hace uso de llaves y de tres variables, las cuales ademas de ser herramientas dentro de supabase, las llamaremos como variables.
-try{
-const {data, error, count} = await supabase.from('manifest').select('id_manifest, name, weight, date, url', {count:'exact'}).order('date',{ascending:false}).range(start, end)
+  try{
+
+    const {data, error, count} = await access.from('manifest').select('id_manifest, name, weight, date, url', {count:'exact'}).order('date', {ascending: false}).range(start, end);
+    
+
+    if(error){
+      throw error
+    }
+
+    const totalRows = Math.ceil(count / limit);
+
+    res.json({data, totalRows});
+
+  }catch(error){
+    console.error(error)
+    res.status(500).json({error: 'error en la obtencion de datos'})
+  }
+
+})
+
+// subida de archivos | INVOICES
+
+app.post('/invoice-table-post',async(req, res)=>{
+  
+  myStorage.array('pdf', 15)(req, res, async(err)=>{
+
+    if(err){
+     return res.status(400).json({message:'Error, solo se permite la carga de maximo 15 archivos'})
+    }
+
+    try{
+          const files = req.files;
+//validacion para confirmar que lo que reciba si tenga contenido, de lo contrario abortar
+
+if(!files || files.length === 0){
+ return res.status(400).json({error:'S'})
+};
+
+const promesaUpload = files.map(async file =>{
+ // Nombre de cada archivo 
+const fileName = Date.now()+'-'+file.originalname;
+
+// Una vez llegan los archivos iteramos con el map, ahora viene la accion de subida al storage primero
+
+const {data, error} = await access.storage.from('invoices').upload(fileName, file.buffer, {contentType: file.mimetype});
+
 if(error){
   throw error
 }
-const totalRows = Math.ceil(count / limit)
-console.log(count)
-console.log(totalRows)
 
-res.json({data, totalRows})
+// obtencion de url 
 
-}catch(error){
-  console.error(error)
-  res.status(500).json({error:'Registros no encontrados'})
+const {data: urlFile} = await access.storage.from('invoices').getPublicUrl(fileName);
+
+const publicUrl = urlFile.publicUrl;
+
+// inserta de los datos en la tabla
+
+return {
+  name: fileName,
+  weight: file.size,
+  date: new Date(),
+  url: publicUrl
 }
 
-
 })
 
-// a Partir de aqui todo lo que venga sera lo enviado a la base de datos, especificamente la accion de publicar los archivos y datos en la BD.
-
-app.post('/manifest-table-post', (req, res)=>{
-  upload.array('pdf', 15)(req, res, async(error)=>{
-    if(error){
-     return res.status(400).json({error:'Solo se permiten 15 archivos como maximo'})
+const resultados = await Promise.all(promesaUpload);
+await access.from('invoices').insert(resultados);
+res.redirect('/sections/loadFile.html')
+    } catch(error){
+      console.error(error);
+      res.status(500).json({error: 'Error de subida de archivo'})
     }
-
-    const files = req.files;
-
-    if(!files && files.length === 0){
-     return res.status(400).json({error:'No se seleccionaron archivos'})
-    }
-
-    const promesa = files.map(async file =>{
-      try{
-        const fileName = Date.now()+'-'+file.originalname
-
-      const {data, error} = await supabase.storage.from('manifest').upload(fileName, file.buffer, {contentType: file.mimetype});
-
-      if(error){
-        throw error;
-      }
-
-      const {data: fileUrl} = await supabase.storage.from('manifest').getPublicUrl(fileName);
-
-      const publicUrl = fileUrl.publicUrl
-
-      return {
-        name:file.originalname,
-        weight: file.size,
-        date: new Date(),
-        url: publicUrl
-      }
-
-      } catch(error){
-        console.log(error)
-        res.status(500).json({error:'Error o fallo imprevisto en la carga de documentos'})
-      }
-
-    })
-
-   const results = await Promise.all(promesa)
-   await supabase.from('manifest').insert(results)
-   res.redirect('/sections/loadManiefst.html')
 
   })
+
 })
 
-// Accion de borrado de registro
-app.delete('/action-delete-manifest/:id', async(req, res)=>{
-  const deleteActionId = req.params.id
-  const {data, error} = await supabase.from('manifest').delete().eq('id_manifest', deleteActionId)
+// Subida de archivos | MANIFIESTOS
 
-  if(error){
-    return res.status(400).json({error: error.message})
+app.post('/manifest-table-post',async(req, res)=>{
+  
+  myStorage.array('pdf', 15)(req, res, async(err)=>{
+
+    if(err){
+     return res.status(400).json({message:'Error, solo se permite la carga de maximo 15 archivos'})
+    }
+
+    try{
+          const files = req.files;
+//validacion para confirmar que lo que reciba si tenga contenido, de lo contrario abortar
+
+if(!files || files.length === 0){
+ return res.status(400).json({error:'S'})
+};
+
+const promesaUpload = files.map(async file =>{
+ // Nombre de cada archivo 
+const fileName = Date.now()+'-'+file.originalname;
+
+// Una vez llegan los archivos iteramos con el map, ahora viene la accion de subida al storage primero
+
+const {data, error} = await access.storage.from('manifest').upload(fileName, file.buffer, {contentType: file.mimetype});
+
+if(error){
+  throw error
+}
+
+// obtencion de url 
+
+const {data: urlFile} = await access.storage.from('manifest').getPublicUrl(fileName);
+
+const publicUrl = urlFile.publicUrl;
+
+// inserta de los datos en la tabla
+
+return {
+  name: fileName,
+  weight: file.size,
+  date: new Date(),
+  url: publicUrl
+}
+
+})
+
+const resultados = await Promise.all(promesaUpload);
+await access.from('manifest').insert(resultados);
+res.redirect('/sections/loadManiefst.html')
+    } catch(error){
+      console.error(error);
+      res.status(500).json({error: 'Error de subida de archivo'})
+    }
+
+  })
+
+})
+
+// ------------------------------------------- //
+//           ELIMINACION DE ARCHIVOS
+// ------------------------------------------- //
+
+// Invoices
+
+app.delete('/action-delete/:id', async(req, res)=>{
+
+  const id = req.params.id;
+  const {data: getFileName, error: getFileNameError} = await access.from('invoices').select('name').eq('id_invoice', id).single()
+  const fileName = getFileName.name
+if(getFileNameError){
+    console.error(getFileNameError)
+    return res.status(400).json({error:getFileNameError.message})
   }
+  const {data, error} = await access.from('invoices').delete().eq('id_invoices', id);
+  const {data: removeFile, error: removeFileError} = await access.storage.from('invoices').remove([fileName])
+  if(error){
+    console.error(error)
+    return res.status(400).json({error:error.message})
+  }
+   
+  res.status(200).send('registro borrado con exito')
 
-  res.status(200).send({message:'registro Borrado con exito'})
-  
 })
-  
 
-  app.listen(3000, ()=>{
-    console.log('servidor operando en el puerto http://localhost:3000')
-  })
+// Manifest
+
+app.delete('/action-delete-manifest/:id', async(req, res)=>{
+
+  const id = req.params.id;
+  const {data: getFileName, error: getFileNameError} = await access.from('manifest').select('name').eq('id_manifest', id).single()
+  const fileName = getFileName.name
+if(getFileNameError){
+    console.error(getFileNameError)
+    return res.status(400).json({error:getFileNameError.message})
+  }
+  const {data, error} = await access.from('manifest').delete().eq('id_manifest', id);
+  const {data: removeFile, error: removeFileError} = await access.storage.from('manifest').remove([fileName])
+  if(error){
+    console.error(error)
+    return res.status(400).json({error:error.message})
+  }
+   
+  res.status(200).send('registro borrado con exito')
+
+})
+
+// ------------------------------------------- //
+//                  Escucha
+// ------------------------------------------- //
+
+app.listen(3000, ()=>{
+  console.log('servidor escuchandose en http://localhost:3000');
+})
+
